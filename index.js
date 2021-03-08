6e89240c2d27daa621f275cfe11dc3c418b097c5
jim <jbmbroomfield@gmail.com>
2021-03-08 17:22:19 +1100

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCard(name,occasion) {
+    return `Thank you, ${name}, for the wonderful ${occasion} gift!`
+} 
+
+function writeCards(names,occasion) {
+    let r = [];
+    for (let i = 0; i < names.length; i++) {
+        r.push(writeCard(names[i],occasion));
+    }
+    return r;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
